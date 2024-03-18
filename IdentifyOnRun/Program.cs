edca50ebd2f30b6da76f670188a558093811daf1
Stuart Walker <stuartw1@gmail.com>
2024-03-18 13:14:44 +0000

    Add project files.

---
+++ IdentifyOnRun/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+string envUserName = Environment.UserName;
+string output = String.Format("Identified {0} {1} {2}", DateTime.Now, userName, envUserName);
+
+Console.WriteLine(output);
+
+string fileName = String.Format(".\\{0}.txt",envUserName);
+
+using (StreamWriter writer = new StreamWriter(fileName))
+{
+    writer.WriteLine(output);
+}
